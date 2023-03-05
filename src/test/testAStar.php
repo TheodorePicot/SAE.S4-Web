7e7ef420bd1071dec2fddb7e5d8a3629ed66ca01
michelu <ugo.michel01@etu.umontpellier.fr>
2023-03-05 10:20:52 +0100

    debut test

---
+++ src/test/testAStar.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class UneClasseTest extends TestCase
+{
+
+    /**
+     * @before
+     */
+    public function initTestEnvironment()
+    {
+        // cette méthode est exécutée avant chaque test
+    }
+
+    /**
+     * @after
+     */
+    public function destroyTestEnvironment()
+    {
+        // cette méthode est exécutée après chaque test
+    }
+
+    public function testMethode()
+    {
+        // la méthode de test
+    }
+}
