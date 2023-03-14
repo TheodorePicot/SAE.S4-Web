375a6be05cdb927228793ed1a59c6a398b3c1055
Jihan <jihanhajem@gmail.com>
2023-03-14 16:17:16 +0100

    burger menu

--- src/vue/vueGenerale.php
+++ src/vue/vueGenerale.php
@@ -23,22 +23,15 @@ $loginURL = rawurlencode(ConnexionUtilisateur::getLoginUtilisateurConnecte()); ?
 <body>
 
 <!-- Optional JavaScript -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
-
 <header>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light mx-4 fs-5 ">
         <a href="#" class="navbar-brand">Navbar</a>
         <button class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around" type="button"
-                data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="toggler-icon top-bar"></span>
             <span class="toggler-icon middle-bar"></span>
@@ -69,11 +62,11 @@ $loginURL = rawurlencode(ConnexionUtilisateur::getLoginUtilisateurConnecte()); ?
                         </a>
                     </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="controleurFrontal.php?action=deconnecter&controleur=utilisateur">
-                        <img alt="logout" src="../ressources/img/logout.png" width="18">
-                    </a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="controleurFrontal.php?action=deconnecter&controleur=utilisateur">
+                            <img alt="logout" src="../ressources/img/logout.png" width="18">
+                        </a>
+                    </li>
                 <?php endif ?>
             </ul>
 
