ef11afa5c898528202bbeaa4ce2c3c106512c584
michell <leo.michel02@etu.umontpellier.fr>
2023-03-21 22:23:01 +0100

    fin exo3 TD1

--- web/controleurFrontal.php
+++ web/controleurFrontal.php
@@ -11,27 +11,21 @@
 //$loader->register();
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\PlusCourtChemin\Controleur\ControleurGenerique;
-
-$c = new \App\PlusCourtChemin\Controleur\ControleurUtilisateur();
-var_dump($c::class);
-
-
 // Syntaxe alternative
 // The null coalescing operator returns its first operand if it exists and is not null
-$action = $_REQUEST['action'] ?? 'afficherListe';
-
-$controleur = $_REQUEST['controleur'] ?? "noeudRoutier";
-
-$controleurClassName = 'App\PlusCourtChemin\Controleur\Controleur' . ucfirst($controleur);
-
-if (class_exists($controleurClassName)) {
-    if (in_array($action, get_class_methods($controleurClassName))) {
-        $controleurClassName::$action();
-    } else {
-        $controleurClassName::afficherErreur("Erreur d'action");
-    }
-} else {
-    ControleurGenerique::afficherErreur("Erreur de contrôleur");
-}
-
+//$action = $_REQUEST['action'] ?? 'afficherListe';
+//
+//$controleur = $_REQUEST['controleur'] ?? "noeudRoutier";
+//
+//$controleurClassName = 'App\PlusCourtChemin\Controleur\Controleur' . ucfirst($controleur);
+//
+//if (class_exists($controleurClassName)) {
+//    if (in_array($action, get_class_methods($controleurClassName))) {
+//        $controleurClassName::$action();
+//    } else {
+//        $controleurClassName::afficherErreur("Erreur d'action");
+//    }
+//} else {
+//    App\PlusCourtChemin\Controleur\ControleurGenerique::afficherErreur("Erreur de contrôleur");
+//}
+App\PlusCourtChemin\Controleur\RouteurQueryString::traiterRequete();
