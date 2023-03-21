30f95cc0d4d0062886a2a4b9e7d17720bde3db77
TheodorePicot <theodore.picot@etu.umontpellier.fr>
2023-03-21 22:08:24 +0100

    fix

--- src/Lib/PlusCourtCheminAStarTroncon.php
+++ src/Lib/PlusCourtCheminAStarTroncon.php
@@ -25,7 +25,7 @@ class PlusCourtCheminAStar
         $tabArrive = $noeudRoutierRepository->getLongitudeLatitude($this->noeudRoutierArriveeGid);
         $tabDepart = $noeudRoutierRepository->getLongitudeLatitude($this->noeudRoutierDepartGid);
 
-        $this->priorityQueue->insert($this->noeudRoutierDepartGid, $this->calculDistanceHeuristque($tabDepart['st_x'], $tabDepart['st_y'], $tabArrive['st_x'], $tabArrive['st_y']) * 1000);
+        $this->priorityQueue->insert($this->noeudRoutierDepartGid, $this->calculDistanceHeuristque($tabDepart['st_x'], $tabDepart['st_y'], $tabArrive['st_x'], $tabArrive['st_y']));
         $this->noeudsALaFrontiere[$this->noeudRoutierDepartGid] = true;
         while (count($this->noeudsALaFrontiere) !== 0) {
             $noeudRoutierGidCourant = $this->priorityQueue->extract();
@@ -43,7 +43,7 @@ class PlusCourtCheminAStar
 
                 if (!isset($this->distances[$noeudVoisinGid]) || $distanceProposee < $this->distances[$noeudVoisinGid]) {
                     $this->distances[$noeudVoisinGid] = $distanceProposee;
-                    $distanceHeuristique = $distanceProposee + ($this->calculDistanceHeuristque($voisin['longitude'], $voisin['latitude'], $tabArrive['st_x'], $tabArrive['st_y']) * 1000);
+                    $distanceHeuristique = $distanceProposee + ($this->calculDistanceHeuristque($voisin['longitude'], $voisin['latitude'], $tabArrive['st_x'], $tabArrive['st_y']));
                     $this->priorityQueue->insert($noeudVoisinGid, $distanceHeuristique);
                     $this->noeudsALaFrontiere[$noeudVoisinGid] = true;
                 }
--- web/controleurFrontal.php
+++ web/controleurFrontal.php
@@ -9,12 +9,13 @@
 //$loader->addNamespace('App\PlusCourtChemin', __DIR__ . '/../src');
 // register the autoloader
 //$loader->register();
+require_once __DIR__ . '/../vendor/autoload.php';
+
 use App\PlusCourtChemin\Controleur\ControleurGenerique;
 
 $c = new \App\PlusCourtChemin\Controleur\ControleurUtilisateur();
 var_dump($c::class);
 
-require_once __DIR__ . '/../vendor/autoload.php';
 
 // Syntaxe alternative
 // The null coalescing operator returns its first operand if it exists and is not null
