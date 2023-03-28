39a87c44a1f1a33cbb3da67bb5cf91a23be01792
michell <leo.michel02@etu.umontpellier.fr>
2023-03-28 17:52:33 +0200

    implementation API météo et test API carbone

--- src/vue/noeudCommune/plusCourtChemin.php
+++ src/vue/noeudCommune/plusCourtChemin.php
@@ -31,7 +31,7 @@
 
                     <!--            <input type="hidden" name="XDEBUG_TRIGGER" value="1">-->
                     <div class="d-flex justify-content-center my-4">
-                        <button type="submit" class="btn btn-light">Calculer</button>
+                        <button id="buttonCalculer" type="submit" class="btn btn-light">Calculer</button>
                     </div>
                 </div>
             </fieldset>
