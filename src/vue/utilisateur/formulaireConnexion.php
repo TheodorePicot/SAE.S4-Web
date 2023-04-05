abba5616fcc66efab8c50b48ef8ff01adb65d448
TheodorePicot <theodore.picot@etu.umontpellier.fr>
2023-04-05 19:03:02 +0200

    fixMDP

--- src/vue/utilisateur/formulaireConnexion.php
+++ src/vue/utilisateur/formulaireConnexion.php
@@ -13,20 +13,20 @@ $assistantUrl = Conteneur::recupererService("assistantUrl");?>
             <div class="my-4">
                 <div class="form-group row">
                     <div class="my-2">
-                        <input type="text" class="form-control" id="login_id" placeholder="Login" name="login">
+                        <label for="login_id"></label><input type="text" class="form-control" id="login_id" placeholder="Login" name="login">
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <div class="my-2">
-                        <input type="password" class="form-control" value="" placeholder="Mot de passe" name="mdp"
-                               id="mdp_id" required>
+                        <label for="mdp_id"></label><input type="password" class="form-control" value="" placeholder="Mot de passe" name="mdp"
+                                                           id="mdp_id" required>
                     </div>
                 </div>
 
 
-                <input type='hidden' name='action' value='connecter'>
-                <input type='hidden' name='controleur' value='utilisateur'>
+<!--                <input type='hidden' name='action' value='connecter'>-->
+<!--                <input type='hidden' name='controleur' value='utilisateur'>-->
                 <div class="d-flex align-content-center justify-content-center">
 
                     <button type="submit" class="btn btn-light my-4">Se connecter</button>
