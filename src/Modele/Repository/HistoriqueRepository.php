58d29d37adc6f15db6561776119e44b6dcfcad42
TheodorePicot <theodore.picot@etu.umontpellier.fr>
2023-04-09 22:39:27 +0200

    fix

--- src/Modele/Repository/HistoriqueRepository.php
+++ src/Modele/Repository/HistoriqueRepository.php
@@ -36,6 +36,21 @@ class HistoriqueRepository extends AbstractRepository implements HistoriqueRepos
         ];
     }
 
+    protected function getNomsColonnesSelect(): array
+    {
+        return [
+            'idTrajet',
+            'login_utilisateur',
+            'comm_depart',
+            'coords_depart',
+            'comm_arrivee',
+            'coords_arrivee',
+            'distance',
+            'coordonnees_du_chemin',
+            'date',
+        ];
+    }
+
     protected function construireDepuisTableau(array $objetFormatTableau): AbstractDataObject
     {
          return new Trajet(
@@ -50,5 +65,29 @@ class HistoriqueRepository extends AbstractRepository implements HistoriqueRepos
             $objetFormatTableau["date"],
         );
 
+    }
+
+    public function recupererPar(array $critereSelection, $limit = 200): array
+    {
+        $nomTable = $this->getNomTable();
+        $champsSelect = implode(", ", $this->getNomsColonnesSelect());
+
+        $partiesWhere = array_map(function ($nomcolonne) {
+            return "$nomcolonne = :$nomcolonne";
+        }, array_keys($critereSelection));
+        $whereClause = join(',', $partiesWhere);
+
+        $requeteSQL = <<<SQL
+            SELECT $champsSelect FROM $nomTable WHERE $whereClause LIMIT $limit;
+        SQL;
+        $pdoStatement = $this->connexionBaseDeDonnees->getPdo()->prepare($requeteSQL);
+        $pdoStatement->execute($critereSelection);
+
+        $objets = [];
+        foreach ($pdoStatement as $objetFormatTableau) {
+            $objets[] = $this->construireDepuisTableau($objetFormatTableau);
+        }
+
+        return $objets;
     }
 }
